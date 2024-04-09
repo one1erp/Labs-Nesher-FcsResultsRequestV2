8794dc93e18fdf10ac8275b4135a54fba639520b
one1erp <avigaile@one1.co.il>
2024-04-09 09:04:23 +0300

    Initial commit with additional files

---
+++ FcsResultsRequest.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace FcsResultsRequestV2
+{
+    partial class FcsResultsRequest
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSdgId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSdgId
+            // 
+            this.txtSdgId.Location = new System.Drawing.Point(87, 88);
+            this.txtSdgId.Name = "txtSdgId";
+            this.txtSdgId.Size = new System.Drawing.Size(100, 20);
+            this.txtSdgId.TabIndex = 1;
+            this.txtSdgId.TextChanged += new System.EventHandler(this.txtSdgId_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(102, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "sdg_id הכנס";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(119, 140);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 13);
+            this.lblMsg.TabIndex = 3;
+            // 
+            // FcsResultsRequest
+            // 
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSdgId);
+            this.Name = "FcsResultsRequest";
+            this.Size = new System.Drawing.Size(302, 192);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSdgId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
---
+++ FcsResultsRequest.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using LSEXT;
+using LSExtensionControlLib;
+using LSExtensionWindowLib;
+using LSSERVICEPROVIDERLib;
+using System.Reflection;
+using System.IO;
+using System.Xml;
+using DAL;
+using Oracle.DataAccess.Client;
+using Common;
+using System.Net;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace FcsResultsRequestV2
+{
+    [ComVisible(true)]
+    [ProgId("FcsResultsRequestV2.FcsResultsRequestV2")]
+
+    public partial class FcsResultsRequest : UserControl
+    //public partial class FcsResultsRequest : UserControl, IExtensionWindow
+    {
+
+        public IDataLayer _dal = null;
+        //private IExtensionWindowSite2 _ntlsSite;
+        //private NautilusServiceProvider _sp;
+        //private NautilusProcessXML _processXml;
+        //private NautilusUser _ntlsUser;
+        //private INautilusDBConnection _ntlsCon;
+        //private OracleCommand cmd;
+        //public bool DEBUG;
+
+        public FcsResultsRequest()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSdgId_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var reportName = (txtSdgId.Text);
+                COA_Report c = _dal.GetCoaReportByName(reportName);
+                if (c != null)
+                {
+                    ResultRequest(c);
+                }
+            }
+        }
+
+        public void ResultRequest(COA_Report coaReport)
+        {
+            try
+            {
+
+                Debugger.Launch();
+                string fullUrl = "";
+                string apiReqResult = "";
+
+                lblMsg.Text = "Please wait...";
+
+                string url = _dal.GetPhraseByName("UrlService_FCS").PhraseEntries
+                        .FirstOrDefault(p => p.PhraseName == "FcsResultRequest")?.PhraseDescription;
+
+                fullUrl = url + coaReport.COAReportId;
+
+                // API request
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(fullUrl);
+                request.Method = "POST";
+                request.ContentLength = 0;
+                using (WebResponse response = request.GetResponse())
+                {
+                    var a = response;
+                    using (Stream responseStream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+
+                            apiReqResult = reader.ReadToEnd();  
+
+                        }
+                    }
+                }
+
+                lblMsg.Text = apiReqResult;
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        //#region Page Extension Interface
+
+        //public void SetSite(object site)
+        //{
+        //    _ntlsSite = (IExtensionWindowSite2)site;
+        //    _ntlsSite.SetWindowInternalName("NautExtExample Event");
+        //    _ntlsSite.SetWindowRegistryName("NautExtExample Event");
+        //    _ntlsSite.SetWindowTitle("NautExtExample Event");
+        //}
+
+        //public void SetServiceProvider(object serviceProvider)
+        //{
+        //    _sp = serviceProvider as NautilusServiceProvider;
+
+        //}
+
+        //public WindowButtonsType GetButtons()
+        //{
+        //    return LSExtensionWindowLib.WindowButtonsType.windowButtonsNone;
+        //}
+
+        //public bool SaveData()
+        //{
+        //    return false;
+        //}
+
+        //public bool CloseQuery()
+        //{
+        //    return true;
+        //}
+
+        //public void Internationalise()
+        //{
+        //    //   throw new NotImplementedException();
+        //}
+
+        //public void PreDisplay()
+        //{
+        //    if (!DEBUG)
+        //    {
+        //        _processXml = _sp.QueryServiceProvider("ProcessXML") as NautilusProcessXML;
+        //        _ntlsUser = _sp.QueryServiceProvider("User") as NautilusUser;
+        //        _ntlsCon = _sp.QueryServiceProvider("DBConnection") as NautilusDBConnection;
+
+        //        Utils.CreateConstring(_ntlsCon);
+        //        _dal = new DataLayer();
+        //        _dal.Connect();
+        //    }
+        //    else
+        //    {
+        //        _processXml = null;
+
+        //        _dal = new MockDataLayer();
+        //        _dal.Connect();
+        //    }
+
+        //    //  throw new NotImplementedException();
+        //}
+
+
+        //public void SetParameters(string parameters)
+        //{
+        //    //  throw new NotImplementedException();
+        //}
+
+
+        //public void Setup()
+        //{
+        //}
+
+
+        //public WindowRefreshType DataChange()
+        //{
+        //    return LSExtensionWindowLib.WindowRefreshType.windowRefreshNone;
+        //}
+
+        //public WindowRefreshType ViewRefresh()
+        //{
+        //    return LSExtensionWindowLib.WindowRefreshType.windowRefreshNone;
+        //}
+
+        //public void refresh()
+        //{
+        //    //    throw new NotImplementedException();
+        //}
+
+        //public void SaveSettings(int hKey)
+        //{
+        //    //  throw new NotImplementedException();
+        //}
+
+        //public void RestoreSettings(int hKey)
+        //{
+        //    //    throw new NotImplementedException();
+        //}
+
+        //public OracleConnection GetConnection(INautilusDBConnection ntlsCon)
+        //{
+
+        //    OracleConnection connection = null;
+
+        //    if (ntlsCon != null)
+        //    {
+
+
+        //        // Initialize variables
+        //        String roleCommand;
+        //        // Try/Catch block
+        //        try
+        //        {
+
+
+
+        //            var _connectionString = ntlsCon.GetADOConnectionString();
+
+        //            var splited = _connectionString.Split(';');
+
+        //            var cs = "";
+
+        //            for (int i = 1; i < splited.Count(); i++)
+        //            {
+        //                cs += splited[i] + ';';
+        //            }
+
+        //            var username = ntlsCon.GetUsername();
+        //            if (string.IsNullOrEmpty(username))
+        //            {
+        //                var serverDetails = ntlsCon.GetServerDetails();
+        //                cs = "User Id=/;Data Source=" + serverDetails + ";";
+        //            }
+
+
+        //            //Create the connection
+        //            connection = new OracleConnection(cs);
+
+        //            // Open the connection
+        //            connection.Open();
+
+        //            // Get lims user password
+        //            string limsUserPassword = ntlsCon.GetLimsUserPwd();
+
+        //            // Set role lims user
+        //            if (limsUserPassword == "")
+        //            {
+        //                // LIMS_USER is not password protected
+        //                roleCommand = "set role lims_user";
+        //            }
+        //            else
+        //            {
+        //                // LIMS_USER is password protected.
+        //                roleCommand = "set role lims_user identified by " + limsUserPassword;
+        //            }
+
+        //            // set the Oracle user for this connecition
+        //            cmd = new OracleCommand(roleCommand, connection);
+
+        //            // Try/Catch block
+        //            try
+        //            {
+        //                // Execute the command
+        //                cmd.ExecuteNonQuery();
+        //            }
+        //            catch (Exception f)
+        //            {
+        //                // Throw the exception
+        //                throw new Exception("Inconsistent role Security : " + f.Message);
+        //            }
+
+        //            // Get the session id
+        //            var sessionId = _ntlsCon.GetSessionId();
+
+        //            // Connect to the same session
+        //            string sSql = string.Format("call lims.lims_env.connect_same_session({0})", sessionId);
+
+        //            // Build the command
+        //            cmd = new OracleCommand(sSql, connection);
+
+        //            // Execute the command
+        //            cmd.ExecuteNonQuery();
+
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            // Throw the exception
+        //            throw e;
+        //        }
+
+        //        // Return the connection
+        //    }
+
+        //    return connection;
+
+        //}
+        //#endregion Page Extension Interface
+
+        //public void Execute(ref LSExtensionParameters Parameters)
+        //{
+
+        //    INautilusServiceProvider _sp;
+        //    //OracleCommand cmd = null;
+        //    //ReportDocument cr = null;
+        //    //Debugger.Launch();
+        //    try
+        //    {
+        //        _sp = Parameters["SERVICE_PROVIDER"];
+        //        string tableName = Parameters["TABLE_NAME"];
+
+        //        int workstation = Parameters["WORKSTATION_ID"];
+        //        //  Debugger.Launch();
+        //        long wnid = Parameters["WORKFLOW_NODE_ID"];
+        //        INautilusDBConnection _ntlsCon = null;
+        //        if (_sp != null)
+        //        {
+        //            _ntlsCon = _sp.QueryServiceProvider("DBConnection") as NautilusDBConnection;
+        //        }
+        //        else
+        //        {
+        //            _ntlsCon = null;
+        //        }
+        //        if (_ntlsCon != null)
+        //        {
+        //            // _username= dbConnection.GetUsername();
+
+        //        }
+
+        //        var records = Parameters["RECORDS"];
+        //        var recordId = records.Fields[tableName + "_ID"].Value;
+        //    }
+        //    catch (Exception EXP)
+        //    {
+        //        MessageBox.Show(EXP.Message);
+
+        //    }
+        //}
+
+
+        private void txtSdgId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
+
+
+
